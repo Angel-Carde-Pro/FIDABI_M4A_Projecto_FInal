52467ff057e478f49f265e233ed04f0e1b95eaf0
Lokyre <joseluisfaresguzman5@gmail.com>
2023-07-14 18:40:02 -0500

    Login request y response

--- app/src/main/java/com/example/fidabi_m4a_projecto_final/models/user.java
+++
@@ -1,4 +0,0 @@
-package com.example.fidabi_m4a_projecto_final.models;
-
-public class user {
-}
--- app/src/main/java/com/example/fidabi_m4a_projecto_final/request/LoginResponse.java
+++ app/src/main/java/com/example/fidabi_m4a_projecto_final/request/LoginResponse.java
@@ -1,49 +1,185 @@
 package com.example.fidabi_m4a_projecto_final.request;
 
-public class LoginResponse {
-  String perPrimerNom;
-  String perSegundoNom;
-  String perTelefono;
-  String rol_nombre;
-  String rol_estado;
+import java.io.Serializable;
+import java.util.List;
 
-    public String getPerPrimerNom() {
-        return perPrimerNom;
+public class LoginResponse implements Serializable {
+    private int usu_cod;
+    private Persona persona;
+    private List<Rol> roles;
+    private String usuario;
+    private String contrasenia;
+    private boolean usu_estado;
+
+    public int getUsuCod() {
+        return usu_cod;
     }
 
-    public void setPerPrimerNom(String perPrimerNom) {
-        this.perPrimerNom = perPrimerNom;
+    public void setUsuCod(int usu_cod) {
+        this.usu_cod = usu_cod;
     }
 
-    public String getPerSegundoNom() {
-        return perSegundoNom;
+    public Persona getPersona() {
+        return persona;
     }
 
-    public void setPerSegundoNom(String perSegundoNom) {
-        this.perSegundoNom = perSegundoNom;
+    public void setPersona(Persona persona) {
+        this.persona = persona;
     }
 
-    public String getPerTelefono() {
-        return perTelefono;
+    public List<Rol> getRoles() {
+        return roles;
     }
 
-    public void setPerTelefono(String perTelefono) {
-        this.perTelefono = perTelefono;
+    public void setRoles(List<Rol> roles) {
+        this.roles = roles;
     }
 
-    public String getRol_nombre() {
-        return rol_nombre;
+    public String getUsuario() {
+        return usuario;
     }
 
-    public void setRol_nombre(String rol_nombre) {
-        this.rol_nombre = rol_nombre;
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
     }
 
-    public String getRol_estado() {
-        return rol_estado;
+    public String getContrasenia() {
+        return contrasenia;
     }
 
-    public void setRol_estado(String rol_estado) {
-        this.rol_estado = rol_estado;
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public boolean isUsuEstado() {
+        return usu_estado;
+    }
+
+    public void setUsuEstado(boolean usu_estado) {
+        this.usu_estado = usu_estado;
+    }
+
+    public static class Persona {
+        private int perCod;
+        private String perCedula;
+        private String perPrimerNom;
+        private String perSegundoNom;
+        private String perApellidoPater;
+        private String perApellidoMater;
+        private String perTelefono;
+        private String perGenero;
+        private String perEmail;
+        private String perFechaNac;
+
+        public int getPerCod() {
+            return perCod;
+        }
+
+        public void setPerCod(int perCod) {
+            this.perCod = perCod;
+        }
+
+        public String getPerCedula() {
+            return perCedula;
+        }
+
+        public void setPerCedula(String perCedula) {
+            this.perCedula = perCedula;
+        }
+
+        public String getPerPrimerNom() {
+            return perPrimerNom;
+        }
+
+        public void setPerPrimerNom(String perPrimerNom) {
+            this.perPrimerNom = perPrimerNom;
+        }
+
+        public String getPerSegundoNom() {
+            return perSegundoNom;
+        }
+
+        public void setPerSegundoNom(String perSegundoNom) {
+            this.perSegundoNom = perSegundoNom;
+        }
+
+        public String getPerApellidoPater() {
+            return perApellidoPater;
+        }
+
+        public void setPerApellidoPater(String perApellidoPater) {
+            this.perApellidoPater = perApellidoPater;
+        }
+
+        public String getPerApellidoMater() {
+            return perApellidoMater;
+        }
+
+        public void setPerApellidoMater(String perApellidoMater) {
+            this.perApellidoMater = perApellidoMater;
+        }
+
+        public String getPerTelefono() {
+            return perTelefono;
+        }
+
+        public void setPerTelefono(String perTelefono) {
+            this.perTelefono = perTelefono;
+        }
+
+        public String getPerGenero() {
+            return perGenero;
+        }
+
+        public void setPerGenero(String perGenero) {
+            this.perGenero = perGenero;
+        }
+
+        public String getPerEmail() {
+            return perEmail;
+        }
+
+        public void setPerEmail(String perEmail) {
+            this.perEmail = perEmail;
+        }
+
+        public String getPerFechaNac() {
+            return perFechaNac;
+        }
+
+        public void setPerFechaNac(String perFechaNac) {
+            this.perFechaNac = perFechaNac;
+        }
+    }
+
+    public static class Rol {
+        private int rol_cod;
+        private String rol_nombre;
+        private boolean rol_estado;
+
+        public int getRolCod() {
+            return rol_cod;
+        }
+
+        public void setRolCod(int rol_cod) {
+            this.rol_cod = rol_cod;
+        }
+
+        public String getRolNombre() {
+            return rol_nombre;
+        }
+
+        public void setRolNombre(String rol_nombre) {
+            this.rol_nombre = rol_nombre;
+        }
+
+        public boolean isRolEstado() {
+            return rol_estado;
+        }
+
+        public void setRolEstado(boolean rol_estado) {
+            this.rol_estado = rol_estado;
+        }
     }
 }
+
