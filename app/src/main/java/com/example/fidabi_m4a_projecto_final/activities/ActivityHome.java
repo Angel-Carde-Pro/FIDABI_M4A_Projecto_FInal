a9e8f9b35fff81a4caf9f1306df99aef6f17a992
Luis Duchi <luis.duchi.est@tecazuay.edu.ec>
2023-07-18 18:55:06 -0500

    codigo qr escanner completo

--- app/src/main/java/com/example/fidabi_m4a_projecto_final/activities/ActivityHome.java
+++ app/src/main/java/com/example/fidabi_m4a_projecto_final/activities/ActivityHome.java
@@ -82,7 +82,7 @@ public class ActivityHome extends AppCompatActivity {
             }
         });
     }
-
+//holaaa
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
