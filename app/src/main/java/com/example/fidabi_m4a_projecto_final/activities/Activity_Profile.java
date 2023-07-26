e24c98cdc9aa4f359762b5cbae2ec53655f07c20
AngelInsta <angel.cardenas.est@tecazuay.edu.ec>
2023-07-25 21:21:38 -0500

    XD

--- app/src/main/java/com/example/fidabi_m4a_projecto_final/activities/Activity_Profile.java
+++ app/src/main/java/com/example/fidabi_m4a_projecto_final/activities/Activity_Profile.java
@@ -1,8 +1,11 @@
 package com.example.fidabi_m4a_projecto_final.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -10,8 +13,10 @@ import android.widget.RelativeLayout;
 import com.example.fidabi_m4a_projecto_final.GlobalData;
 import com.example.fidabi_m4a_projecto_final.R;
 
+import kotlin.jvm.internal.Intrinsics;
+
 public class Activity_Profile extends AppCompatActivity {
-    RelativeLayout signup, setting, shareApp;
+    RelativeLayout signup, setting, shareApp, visit_play_store;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,8 @@ public class Activity_Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         signup = findViewById(R.id.log_out);
         setting = findViewById(R.id.ajustes);
+        shareApp = findViewById(R.id.share_app);
+        visit_play_store = findViewById(R.id.play_store);
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +45,49 @@ public class Activity_Profile extends AppCompatActivity {
                 startActivity(timer);
             }
         });
+
+        shareApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Llama al método shareApp() cuando se haga clic en tu LinearLayout
+                shareApp();
+            }
+        });
+
+        visit_play_store.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Llama al método shareApp() cuando se haga clic en tu LinearLayout
+                openAppInPlayStore();
+            }
+        });
     }
+
+    public final void shareApp() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getString(R.string.base_link_apk));
+        sb.append(getPackageName());
+        String sb2 = sb.toString();
+        String string = getString(R.string.app_name);
+        Intent intent = new Intent("android.intent.action.SEND");
+        intent.setType("text/plain");
+        intent.putExtra("android.intent.extra.SUBJECT", getString(R.string.app_name));
+        StringBuilder sb3 = new StringBuilder();
+        sb3.append(string);
+        sb3.append(" : ");
+        sb3.append(getString(R.string.app_name));
+        sb3.append(": ");
+        sb3.append(sb2);
+        intent.putExtra("android.intent.extra.TEXT", sb3.toString());
+        startActivity(Intent.createChooser(intent, getString(R.string.share_via)));
+    }
+
+    public void openAppInPlayStore() {
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + getPackageName())));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + getPackageName())));
+        }
+    }
+
 }
